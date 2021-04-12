ecf408df14952de5cb647fcf0ca42cfb39667474
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-04-12 11:25:49 +0700

    showin all products data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react"
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    (async () => {
+      const data = await fetch('https://izzstore.herokuapp.com/api/products')
+        .then((res) => res.json())
+        .then((data) => data)
+      console.log(data)
+      setProducts(data)
+    })()
+
+    return () => { }
+  }, [])
+
+  return (
+    <div>
+      <div className="title">Store</div>
+      <div className="product-list">
+        {
+          products.length ? products.map((product, index) => <p key={index}>{`${product.name} - ${product.quantity} - $${product.price}`}</p>) : 'Loading data...'
+        }
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
