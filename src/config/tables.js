cb0a143ef51319bc0aead39fd3526ab269b2923c
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-04-13 10:49:58 +0700

    rename src folder become server

--- src/config/tables.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  setupQueries: [
-    'CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, quantity INTEGER NOT NULL, price NUMERIC(5,2));'
-  ]
-}
